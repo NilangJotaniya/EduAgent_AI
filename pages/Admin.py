1a9b9cf6a5d7698eac7bd5d5f5c583c9b7f9a1e8
Nilang Jotaniya <nilang2484@gmail.com>
2026-02-22 17:35:49 +0530

    Delete pages/Admin.py

--- pages/Admin.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("⚙️ Admin Dashboard")
-
-new_doc = st.text_area("Add Academic Knowledge")
-
-if st.button("Add Knowledge"):
-    st.success("Knowledge added (we will connect vector DB next)")
